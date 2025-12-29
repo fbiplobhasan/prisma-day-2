e35e19b198f591ffbef8ef4f56fe550839165af0
Biplob Hasan <your-email@example.com>
2025-12-30 00:20:20 +0600

    add fourth commit

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,11 +1,19 @@
-import express, { Router } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
 import { postController } from "./post.controller";
+import { auth as betterAuth } from "../../lib/auth";
 
 const router = express.Router();
 
-router.post(
-    "/",
-    postController.createPost
-);
+const auth = (...role: any[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    // console.log("middleware checker...", role);
+    const session = await betterAuth.api.getSession({
+      headers: req.headers as any,
+    });
+    console.log(session)
+  };
+};
+
+router.post("/", auth("Admin", "User"), postController.createPost);
 
 export const postRouter: Router = router;
