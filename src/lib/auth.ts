4d55ce5f3dc3e1411013f1c82cc26a3529739965
Biplob Hasan <your-email@example.com>
2025-12-29 23:58:19 +0600

    day-2 third command

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -75,6 +75,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
+      prompt: "select_account consent",
+      accessType: "offline",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
