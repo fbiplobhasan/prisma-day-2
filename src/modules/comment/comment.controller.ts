d7f99e04c1589d9c42a3c1f40250daa8d0c62920
Biplob Hasan <your-email@example.com>
2026-01-06 09:42:18 +0600

    add finished module 27

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -13,6 +13,72 @@ const createComment = async (req: Request, res: Response) => {
   }
 };
 
+const getCommentById = async (req: Request, res: Response) => {
+  try {
+    const { commentId } = req.params;
+    const result = await commentService.getCommentById(commentId as string);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Comment creation failed.",
+      details: error,
+    });
+  }
+};
+
+const getCommentByAuthor = async (req: Request, res: Response) => {
+  try {
+    const { authorId } = req.params;
+    const result = await commentService.getCommentByAuthor(authorId as string);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Comment creation failed.",
+      details: error,
+    });
+  }
+};
+
+const deleteComment = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    const { commentId } = req.params;
+    const result = await commentService.deleteComment(
+      commentId as string,
+      user?.id as string
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "delete failed.",
+      details: error,
+    });
+  }
+};
+
+const updateComment = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    const { commentId } = req.params;
+    const result = await commentService.updateComment(
+      commentId as string,
+      req.body,
+      user?.id as string
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Comment update failed!",
+      details: error,
+    });
+  }
+};
+
 export const commentController = {
   createComment,
+  getCommentById,
+  getCommentByAuthor,
+  deleteComment,
+  updateComment
 };
--- src/modules/comment/comment.router.ts
+++ src/modules/comment/comment.router.ts
@@ -4,6 +4,22 @@ import auth, { UserRole } from "../../middlewares/auth";
 
 const router = express.Router();
 
+router.get("/:commentId", commentController.getCommentById);
+
+router.get("/author/:authorId", commentController.getCommentByAuthor);
+
+router.delete(
+  "/:commentId",
+  auth(UserRole.ADMIN, UserRole.USER),
+  commentController.deleteComment
+);
+
+router.patch(
+  "/:commentId",
+  auth(UserRole.ADMIN, UserRole.USER),
+  commentController.updateComment
+);
+
 router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.USER),
